686a1a56d8f41f6618fb66dc66b855576df3e1e2
Pritesh Agarwal <pritesh0@pop-os.localdomain>
2023-08-13 23:13:09 +0530

    added script to generate a parameter file

--- getp.py
+++ getp.py
@@ -1,5 +1,11 @@
 from pymavlink import mavutil
 
+pf=open("test.param","a")
+pf.write("# Onboard parameters for Vehicle 1\n")
+pf.write("#\n")
+pf.write("# Stack: ArduPilot\n# Vehicle: Rover\n# Version: 4.2.3\n# Git Revision: 2172cfb3\n#\n# Vehicle-Id Component-Id Name Value Type\n")
+#pf.close()
+
 conn=mavutil.mavlink_connection('tcpin:127.0.0.1:7777')
 
 conn.wait_heartbeat()
@@ -14,8 +20,8 @@ while True:
 
     try:
         msg=conn.recv_match(type='PARAM_VALUE', blocking=True).to_dict()
-        print(len(msg))
-        #print('Name: %s\tValue: %f\tType %d' %(msg['param_id'], msg['param_value'], msg['param_type']))
+        #print(len(msg))
+        pf.write("1\t1\t%s  %f  %d\n" %(msg['param_id'], msg['param_value'], msg['param_type']))
     except Exception as error:
         print(error)
-
+pf.close()
